f9ecaaece2d9890c43e34254880c76abb20179a5
timmli <lichte@phil.hhu.de>
2016-07-27 10:15:38 +0200

    auto-indent when yanking

--- settings/setup-buffer.el
+++ settings/setup-buffer.el
@@ -182,6 +182,26 @@
 
 
 ;;==========================================================
+;;      INDENTATION
+;;==========================================================
+
+;; auto-indent when yanking
+;; https://www.emacswiki.org/emacs/AutoIndentation
+(dolist (command '(yank yank-pop))
+	(eval `(defadvice ,command (after indent-region activate)
+					 (and (not current-prefix-arg)
+								(member major-mode '(emacs-lisp-mode lisp-mode
+																										 clojure-mode    scheme-mode
+																										 haskell-mode    ruby-mode
+																										 rspec-mode      python-mode
+																										 c-mode          c++-mode
+																										 objc-mode       latex-mode
+																										 plain-tex-mode))
+								(let ((mark-even-if-inactive transient-mark-mode))
+									(indent-region (region-beginning) (region-end) nil))))))
+
+
+;;==========================================================
 ;;      SELECTION
 ;;==========================================================
 
