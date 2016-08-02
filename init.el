326e51d441ff4f1d083fc967dd46e8d5cdd6729d
timmli <lichte@phil.hhu.de>
2016-08-02 17:58:33 +0200

    removed Linux Biolinum

--- init.el
+++ init.el
@@ -43,7 +43,8 @@
 (custom-set-faces
  '(default ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
  '(fixed-pitch ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
- '(variable-pitch ((t (:family "Linux Biolinum" :foundry "outline" :slant normal :weight normal :height 135 :width normal)))))
+ '(variable-pitch ((t (:family "DejaVu Sans" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
+ )
 ;; load custom file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (if (file-exists-p custom-file)
