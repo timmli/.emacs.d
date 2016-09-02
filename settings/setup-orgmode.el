63af01d7ecdffaf395659eadf7a93ae6d86eaa6f
Timm Lichte <lichte@phil.hhu.de>
2016-09-02 19:06:43 +0200

    org-mode: nicer bullets

--- settings/setup-orgmode.el
+++ settings/setup-orgmode.el
@@ -70,6 +70,13 @@
 
 (setq org-hide-emphasis-markers t)
 
+;; nicer bullets
+(use-package org-bullets
+	:ensure t
+	:config 
+	(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+	)
+
 
 ;;==========================================================
 ;;      MODULES
