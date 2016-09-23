0d4ace2a81bbead7d2c32fa82597a68e9b19ac20
Timm Lichte <lichte@phil.hhu.de>
2016-09-23 18:48:22 +0200

    added toggle for letter case

--- settings/setup-buffer.el
+++ settings/setup-buffer.el
@@ -492,6 +492,39 @@ Version 2016-06-18"
     (forward-char)
     ))
 
+;; toggle letter case
+(global-set-key (kbd "C-9") 'xah-toggle-letter-case) 
+(defun xah-toggle-letter-case ()
+  "Toggle the letter case of current word or text selection.
+Always cycle in this order: Init Caps, ALL CAPS, all lower.
+
+URL `http://ergoemacs.org/emacs/modernization_upcase-word.html'
+Version 2016-01-08"
+  (interactive)
+  (let (
+        (deactivate-mark nil)
+        -p1 -p2)
+    (if (use-region-p)
+        (setq -p1 (region-beginning)
+              -p2 (region-end))
+      (save-excursion
+        (skip-chars-backward "[:alnum:]")
+        (setq -p1 (point))
+        (skip-chars-forward "[:alnum:]")
+        (setq -p2 (point))))
+    (when (not (eq last-command this-command))
+      (put this-command 'state 0))
+    (cond
+     ((equal 0 (get this-command 'state))
+      (upcase-initials-region -p1 -p2)
+      (put this-command 'state 1))
+     ((equal 1  (get this-command 'state))
+      (upcase-region -p1 -p2)
+      (put this-command 'state 2))
+     ((equal 2 (get this-command 'state))
+      (downcase-region -p1 -p2)
+      (put this-command 'state 0)))))
+
 ;; kill buffer
 (global-set-key (kbd "M-<f4>") 'kill-this-buffer)
 
