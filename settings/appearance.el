bc918c09dc0537abaf8ab8d3abc6ad566a0589a1
Timm Lichte <lichte@phil.hhu.de>
2016-12-09 15:30:35 +0100

    added toggle-proportional

--- settings/appearance.el
+++ settings/appearance.el
@@ -70,4 +70,67 @@
 	)
 (global-set-key (kbd "<f10>") 'writeroom-mode)
 
+
+;; toggle proportional mode when appropriate
+;; inspired by https://ogbe.net/blog/toggle-serif.html
+(defvar font-preserve-default-list nil
+  "A list holding the faces that preserve the default family and
+  height when TOGGLE-SERIF is used.")
+(setq font-preserve-default-list
+      '(;; LaTeX markup
+        font-latex-math-face
+        font-latex-sedate-face
+        font-latex-warning-face
+        ;; org markup
+        org-latex-and-related
+        org-meta-line
+        org-verbatim
+        org-block-begin-line
+        ;; syntax highlighting using font-lock
+        font-lock-builtin-face
+        font-lock-comment-delimiter-face
+        font-lock-comment-face
+        font-lock-constant-face
+        font-lock-doc-face
+        font-lock-function-name-face
+        font-lock-keyword-face
+        font-lock-negation-char-face
+        font-lock-preprocessor-face
+        font-lock-regexp-grouping-backslash
+        font-lock-regexp-grouping-construct
+        font-lock-string-face
+        font-lock-type-face
+        font-lock-variable-name-face
+        font-lock-warning-face))
+(defun toggle-proportional ()
+  "Change the default face of the current buffer to use a proportional family."
+  (interactive)
+  (when (display-graphic-p)  ;; this is only for graphical emacs
+    ;; the serif font familiy and height, save the default attributes
+    (let ((proportional-fam "Segoe UI")
+          (proportional-height 125)
+          (default-fam (face-attribute 'default :family))
+          (default-height (face-attribute 'default :height)))
+      (if (not (bound-and-true-p default-cookie))
+          (progn (make-local-variable 'default-cookie)
+                 (make-local-variable 'preserve-default-cookies-list)
+                 (setq preserve-default-cookies-list nil)
+                 ;; remap default face to serif
+                 (setq default-cookie
+                       (face-remap-add-relative
+                        'default :family proportional-fam :height proportional-height))
+                 ;; keep previously defined monospace fonts the same
+                 (dolist (face font-preserve-default-list)
+                   (add-to-list 'preserve-default-cookies-list
+                                (face-remap-add-relative
+                                 face :family default-fam :height default-height)))
+                 (message "Turned on proportional font."))
+        ;; undo changes
+        (progn (face-remap-remove-relative default-cookie)
+               (dolist (cookie preserve-default-cookies-list)
+                 (face-remap-remove-relative cookie))
+               (setq default-cookie nil)
+               (setq preserve-default-cookies-list nil)
+               (message "Restored default fonts."))))))
+
 (provide 'appearance)
