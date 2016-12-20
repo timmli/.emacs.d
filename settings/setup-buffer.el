25737ced6749ba472967964570d7ab053b9b49ae
timmli <lichte@phil.hhu.de>
2016-12-20 18:13:36 +0100

    company: ignore <return>

--- settings/setup-buffer.el
+++ settings/setup-buffer.el
@@ -90,6 +90,9 @@
 	(eval-after-load 'company
 	  '(progn
 	     (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+			 (define-key company-active-map (kbd "<return>") nil)
+			 (define-key company-active-map (kbd "RET") nil)
+	     ;; (define-key company-active-map (kbd "<tab>") 'company-complete-common)  ; Insert the common part of all candidates.  
 	     ;; (define-key company-active-map (kbd "<tab>") 'company-select-next))
 		))
 	(use-package company-flx
