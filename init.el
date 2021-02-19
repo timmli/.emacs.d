4a8fffc08ad32928348669b98732fd42501a1dc4
timmli <timm.lichte@uni-tuebingen.de>
2021-02-19 23:02:03 +0100

    Add straight.el

--- init.el
+++ init.el
@@ -302,6 +302,21 @@ This variable should be changed in private-emacs-settings.el.")
 (use-package quelpa
 	:ensure t)
 
+;; straight.el: yet another (purely functional) package manager
+;; https://github.com/raxod502/straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;;==========================================================
 ;;      LOAD REMAINING SETTINGS
 ;;==========================================================
