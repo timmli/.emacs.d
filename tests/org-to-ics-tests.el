7edf61fee85520ecba3efa83e166d059af2e47dc
timmli <timm.lichte@uni-tuebingen.de>
2023-01-29 12:53:26 +0100

    Remove superfluous org-timestamps in calendar export

--- tests/org-to-ics-tests.el
+++ tests/org-to-ics-tests.el
@@ -9,20 +9,48 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/org-9.6.1"))
 
 
+(defun tl/remove-org-timestamps ()
+	"Remove all (exportet) Org timestamps and surrounding blanks from buffer."
+	(save-excursion
+		(while (re-search-forward
+						(concat "\\([[:blank:]]\\)*"
+										"\\(" org-element--timestamp-regexp "\\)"
+										"\\(–\\|[[:blank:]]\\)*"
+										) nil t)
+			(replace-match ""))))
+
+(defun tl/org-export-agenda-to-ics ()
+	(interactive)
+	(if (org-agenda-files)
+			(let* (
+						 (org-export-with-broken-links t) ; Ignore broken links
+						 )
+				(org-icalendar-combine-agenda-files) 
+				(message (concat "org-agenda exported to "  org-icalendar-combined-agenda-file))
+				(save-window-excursion
+					(find-file org-icalendar-combined-agenda-file)
+					(beginning-of-buffer)
+					(tl/remove-org-timestamps)
+					(save-buffer)
+					(kill-buffer))
+				(message (concat "org-agenda calendar file cleaned and saved: " org-icalendar-combined-agenda-file)))
+		(message "tl/org-export-agenda-to-ics: org-agenda-files not specified, export aborted.")
+		)
+	)
+
 (defun tl/org-export-buffer-to-ics ()
 	(interactive)
 	(if (buffer-file-name (current-buffer))
 			(let* ((file-name (buffer-file-name (current-buffer)))
-						 (org-export-with-broken-links t) ; Ignore broken links
-						 (org-export-with-date nil)				; TODO: Does not remove superfluous timestamps 
 						 (org-agenda-files (list file-name))
 						 (org-icalendar-combined-agenda-file
 							(concat
 							 (file-name-sans-extension file-name)
 							 ".ics")))
-				(org-icalendar-combine-agenda-files))
+				(tl/org-export-agenda-to-ics))
 		(message "tl/org-export-buffer-to-ics: buffer has no file, export aborted.")
 		))
+
 
 (setq org-icalendar-timezone "Europe/Berlin"
 			org-icalendar-include-todo nil			; Non-nil means create VTODO components from TODO items.
