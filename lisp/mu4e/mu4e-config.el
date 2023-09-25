36b619edb109e0943e0fc49538c9024f1cbc3e5f
timmli <timm.lichte@uni-tuebingen.de>
2023-09-25 09:43:31 +0200

    Update mu4e to v1.10.7

--- lisp/mu4e/mu4e-config.el
+++ lisp/mu4e/mu4e-config.el
@@ -1,12 +1,12 @@
 ;; auto-generated
 
-(defconst mu4e-mu-version "1.10.5"
+(defconst mu4e-mu-version "1.10.7"
   "Required mu binary version; mu4e's version must agree with this.")
 
-(defconst mu4e-builddir "/tmp/guix-build-mu-1.10.5.drv-0/build/mu4e"
+(defconst mu4e-builddir "/tmp/guix-build-mu-1.10.7.drv-0/build/mu4e"
   "Top-level build directory.")
 
-(defconst mu4e-doc-dir "/gnu/store/1kdlxl9cr2d8v9x51kgg2z4i8gxwc889-mu-1.10.5/share/doc/mu"
+(defconst mu4e-doc-dir "/gnu/store/3r8qzrrzwn5lqkpqxzx41nsv7lznh4ql-mu-1.10.7/share/doc/mu"
   "Mu4e's data-dir.")
 
 (provide 'mu4e-config)
