eff5a3308bbd1a4a0bbb585ea299fab8523ab9fb
timmli <timm.lichte@uni-tuebingen.de>
2023-12-09 21:39:46 +0100

    helm-khard: Modify tests

--- lisp/helm-khard/tests/helm-khard-test.el
+++ lisp/helm-khard/tests/helm-khard-test.el
@@ -1,7 +1,8 @@
-(require 'helm-khard)
-
-(let ((helm-khard-config-file (expand-file-name (concat (file-name-directory (buffer-file-name))
-																												"khard.conf")))
-			(helm-khard-vdirsyncer-command "echo \"Fake execution of vdirsyncer ...\"")
-			(helm-khard--candidates nil))
+(let* ((helm-khard-test-dir (file-name-directory (buffer-file-name)))
+			 (helm-khard-config-file (expand-file-name (concat helm-khard-test-dir
+																												 "khard.conf")))
+			 (helm-khard-vdirsyncer-command "echo \"Fake execution of vdirsyncer ...\"")
+			 (helm-khard--candidates nil))
+	(setenv "HELM_KHARD_TEST_DIR" helm-khard-test-dir)
+	(require 'helm-khard)
 	(helm-khard))
