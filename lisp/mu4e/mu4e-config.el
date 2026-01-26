5eb7cf759ea75202a3df3eefa7fb338ae85099f7
timmli <timm.lichte@uni-tuebingen.de>
2026-01-26 20:36:23 +0100

    mu4e: Update to v1.12.15

--- lisp/mu4e/mu4e-config.el
+++ lisp/mu4e/mu4e-config.el
@@ -6,11 +6,11 @@
 
 ;;; Code:
 
-(defconst mu4e-mu-version "1.12.14"
+(defconst mu4e-mu-version "1.12.15"
   "Required mu binary version.
 mu4e's version must agree with this.")
 
-(defconst mu4e-doc-dir "/gnu/store/k1l8b2m51sk8pkp8xbdx6b10yxc6mym0-mu-1.12.14/share/doc/mu"
+(defconst mu4e-doc-dir "/gnu/store/vpjhhv5cdv92r2319ihm4ws38ia9wlz3-mu-1.12.15/share/doc/mu"
   "Directory for mu4e documentation.")
 
 (provide 'mu4e-config)
